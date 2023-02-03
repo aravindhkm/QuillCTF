6979d6ae8248136a61cf6f2b15c17d860fc726f0
aravindhkm <aravindhkathiresan@gmail.com>
2023-02-03 06:42:29 +0530

    update

--- TrueXOR/src/TrueXORAttack.sol
+++ TrueXOR/src/TrueXORAttack.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.7;
 
 contract TrueXORAttack {
+    // Validate that the relayer has sent enough gas for the call.
+    // See https://ronan.eth.limo/blog/ethereum-gas-dangers/
     function giveBool() external view returns (bool) {
         if(gasleft() < 1400) return true;
         else return false;
